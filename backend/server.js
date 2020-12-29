160d40dda410c1b1d9f4dc2ea9c811e294028a92
mehdi snene <snene@localhost.localdomain>
2020-12-29 20:29:07 +0100

    heroku  commit

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,17 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 mongoose.connect(
   " mongodb+srv://user:user@cluster0.8kudi.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
 );
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connection to db established"));
+
 app.use(express.json());
 
 app.use("/api/page", require("./routes/Page"));
@@ -19,7 +25,8 @@ app.use("/api/formData", require("./routes/FormData"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
+const PORT = process.env.PORT || process.env.API_PORT;
 
-app.listen(process.env.PORT, () =>
+app.listen({ port: PORT }, () =>
   console.log(`server has started at port ${process.env.PORT}`)
 );
